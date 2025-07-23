404d24e11463cdf86391e7a320af31c97260bb0b
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-07-23 21:17:12 +0200

    Use new registry and upgraded cli flow

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-import { setUser, readConfig } from './config.js';
+import { registerCommand, runCommand, type CommandsRegistry } from './registry.js';
+import { handlerLogin } from './handler_login.js';
 
 function main() {
-  setUser("lane");
-  const cfg = readConfig();
-  console.log(cfg);
+	const args = process.argv.slice(2);
+
+  	if (args.length < 1) {
+    		console.log("usage: cli <command> [args...]");
+    		process.exit(1);
+  	}
+
+	const cmdName = args[0];
+	const cmdArgs = args.slice(1);
+	const registry = {} as CommandsRegistry;
+
+	registerCommand(registry, "login", handlerLogin);
+
+	try {
+    		runCommand(registry, cmdName, ...cmdArgs);
+  	} catch (err) {
+    		if (err instanceof Error) {
+      			console.error(`Error running command ${cmdName}: ${err.message}`);
+    		} else {
+      			console.error(`Error running command ${cmdName}: ${err}`);
+    		}
+    		process.exit(1);
+  	}
 }
 
 main();

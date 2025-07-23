81591f91771a7a94e8c38a4ae3e7a20c9a641428
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-07-23 22:24:08 +0200

    Add new register cmd

---
+++ src/handler_register.ts
@@ -0,0 +1,17 @@
+import { setUser } from "./config";
+import { createUser } from "./lib/db/queries/users";
+
+export async function handlerRegister(cmdName: string, ...args: string[]) {
+  if (args.length != 1) {
+    throw new Error(`usage: ${cmdName} <name>`);
+  }
+
+  const userName = args[0];
+  const user = await createUser(userName);
+  if (!user) {
+    throw new Error(`User ${userName} not found`);
+  }
+
+  setUser(user.name);
+  console.log("User created successfully!");
+}

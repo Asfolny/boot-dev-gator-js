0ba3d57e2ae0cd6d8d9091a103eab3c80e5ce93e
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-07-24 23:42:38 +0200

    Print feed after creating with add feed

--- src/handler_add_feed.ts
+++ src/handler_add_feed.ts
@@ -2,6 +2,8 @@ import { addFeed } from "./lib/db/queries/feeds";
 import { readConfig } from "./config";
 import { getUser } from "./lib/db/queries/users";
 import { Feed, User } from "src/lib/db/schema";
+import { createFeedFollow } from "./lib/db/queries/feed_follows";
+import { printFeedFollow } from "./handler_follow";
 
 export async function handlerAddFeed(cmdName: string, ...args: string[]): Promise<void> {
 	if (args.length < 2) {
@@ -19,8 +21,12 @@ export async function handlerAddFeed(cmdName: string, ...args: string[]): Promis
 
 	const feed = await addFeed(args[0], args[1], user.id);
 	if (!feed) {
+		console.log(feed);
 		throw new Error(`Failed to create feed`);
 	}
+
+	const feedFollow = await createFeedFollow(user.id, feed.id);
+	printFeedFollow(user.name, feedFollow.feed_name);
 
 	console.log("Feed created successfully:");
 	printFeed(feed, user);

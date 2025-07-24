3475b11bc3451ced12261a3aac56ee78d6f7bc2d
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-07-24 23:41:24 +0200

    Add new feed_follows table

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, timestamp, uuid, text } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, text, unique } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom().notNull(),
@@ -18,6 +18,16 @@ export const feeds = pgTable("feeds", {
   url: text("url").notNull().unique(),
   user_id: uuid("user_id").references(() => users.id, { onDelete: "cascade", onUpdate: "cascade" }).notNull(),
 });
+
+export const feedFollows = pgTable("feed_follows", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow().$onUpdate(() => new Date()),
+  feed_id: uuid("feed_id").references(() => feeds.id, { onDelete: "cascade", onUpdate: "cascade" }).notNull(),
+  user_id: uuid("user_id").references(() => users.id, { onDelete: "cascade", onUpdate: "cascade" }).notNull(),
+}, (t) => ({
+  feed_user_uniq: unique("feed_user_uniq").on(t.feed_id, t.user_id),
+}));
 
 
 export type User = typeof users.$inferSelect;

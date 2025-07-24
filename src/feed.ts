d19380a56ef45e5a69e5cb15800d6c252ad4989b
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-07-24 19:13:26 +0200

    Add xml rss feed parsing

---
+++ src/feed.ts
@@ -0,0 +1,63 @@
+import { XMLParser } from "fast-xml-parser";
+
+type RSSFeed = {
+  channel: {
+    title: string;
+    link: string;
+    description: string;
+    item: RSSItem[];
+  };
+};
+
+type RSSItem = {
+  title: string;
+  link: string;
+  description: string;
+  pubDate: string;
+};
+
+export async function fetchFeed(feedURL: string): RSSFeed {
+	const resp = await fetch(feedURL, {
+		headers: {
+			'User-Agent': 'gator'
+		},
+	});
+	const data = await resp.text();
+
+	const parser = new XMLParser();
+	const full = parser.parse(data);
+
+	if (full.rss.channel === undefined) {
+		throw new Error('invalid rss xml format');
+	}
+	const channel = full.rss.channel;
+
+	if (channel.title === undefined || channel.description === undefined || channel.link === undefined) {
+		throw new Error('Failed to find channel metadata');
+	}
+
+	const items = [];
+	if (Array.isArray(channel.item)) {
+		for (const item of channel.item) {
+			if (item.title === undefined || item.link === undefined || item.description === undefined || item.pubDate === undefined) {
+				continue;
+			}
+
+			items.push({
+				title: item.title,
+				link: item.link,
+				description: item.description,
+				pubDate: item.pubDate,
+			});
+		}
+	}
+
+	const feed = {
+		title: channel.title,
+		link: channel.link,
+		description: channel.description,
+		item: items,
+	};
+
+	return feed;
+}

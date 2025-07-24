afbdfb08891d988ad85d1605f14a7fd11ef5ead4
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-07-24 23:04:14 +0200

    Fix type errors

--- src/handler_agg.ts
+++ src/handler_agg.ts
@@ -1,6 +1,6 @@
 import { fetchFeed } from "./feed";
 
-export async function handlerAgg(cmdName: string, ...args: strings[]): Promise<void> {
+export async function handlerAgg(cmdName: string, ...args: string[]): Promise<void> {
 	const feed = await fetchFeed("https://www.wagslane.dev/index.xml");
-	console.log(feed);
+	console.log(feed.channel);
 }

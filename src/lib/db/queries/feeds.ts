c2ba9862d61669fec5302bac20f04f9d4be61ffa
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-07-24 23:44:03 +0200

    Fix addFeed to actually return something

--- src/lib/db/queries/feeds.ts
+++ src/lib/db/queries/feeds.ts
@@ -4,7 +4,7 @@ import { eq } from "drizzle-orm";
 import { firstOrUndefined } from "./utils";
 
 export async function addFeed(name: string, url: string, user_id: string) {
-  const [result] = await db.insert(feeds).values({ name: name, url: url, user_id: user_id });
+  const [result] = await db.insert(feeds).values({ name: name, url: url, user_id: user_id }).returning();
   return result;
 }
 
